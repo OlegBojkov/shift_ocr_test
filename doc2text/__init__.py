9430416a57269dd44fc99f765e340cba9bc00137
Oleg <bojkov.oleg@gmail.com>
2024-12-02 22:45:12 +0300

    Initial commit

---
+++ doc2text/__init__.py
@@ -0,0 +1,5 @@
+from shift_ocr_test.doc2text.reader import Reader
+
+__all__ = [
+    'Reader'
+]
---
+++ generator/__init__.py
@@ -0,0 +1,5 @@
+from shift_ocr_test.generator.generator import Generator
+
+__all__ = [
+    'Generator'
+]
